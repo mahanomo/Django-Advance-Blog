d9145c2150c536efd9a25ebb4d02ce05619a69ee
Mahan Mirdar <mahan.mirdar1381@gmail.com>
2025-05-17 03:01:54 +0330

    Create insert_data.py
    insert_data means use faker and create fake data for create post.

---
+++ core/blog/management/commands/insert_data.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+
+from faker import Faker
+from datetime import datetime
+import random
+
+from accounts.models import User, Profile
+from blog.models import Post, Category
+
+category_list = Category.objects.all()
+cat_list=[]
+for cat in category_list:
+    cat_list.append(cat.name)
+
+class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super(Command,self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+        user_obj= User.objects.create_user(email=self.fake.email(), password="Abc@12345")
+        profile_obj = Profile.objects.get(user=user_obj)
+        profile_obj.first_name = self.fake.first_name()
+        profile_obj.last_name = self.fake.last_name()
+        profile_obj.descreaption = self.fake.paragraph(nb_sentences=1)
+        profile_obj.save()
+        for _ in range(2):
+            Post.objects.create(
+                author = Profile.objects.get(user=user_obj),
+                title = self.fake.paragraph(nb_sentences=1),
+                content = self.fake.paragraph(nb_sentences=9),
+                category = random.choice(category_list),
+                status = random.choice([True,False]),
+                published_date = datetime.now(),
+            )
